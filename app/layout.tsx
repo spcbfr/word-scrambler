532580d11690574ecc586d0df8b7c28cd7ff50d1
Yusuf Bouzekri <youssefbouzekri@protonmail.com>
2023-06-16 21:20:11 +0100

    Update nextJS metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,31 @@
 import './globals.css'
 import { Karla } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
+import { Metadata } from 'next';
+import { title } from 'process';
 
 const karla = Karla({ 
   subsets: ['latin'],
   variable: '--font-karla'
 })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Yusuf's Word Scrambler",
   description: "An app where you can input text and we'll shuffle it around for you",
+  openGraph: {
+    title: "a React text Shuffler",
+    description: "an app that'll shuffle your text for you and allow to download the result",
+    siteName: "scrambledeggs.yusuf.fyi",
+    locale: "en_US",
+    type: "website"
+  },
+  twitter: {
+    card: "summary",
+    title: "a React text shuffler",
+    description: "an app that'll shuffle your text for you and allow to download the result",
+    creator: "@spacehiker_"
+  }
+
 }
 
 export default function RootLayout({
